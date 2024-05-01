9392de292cecd66c47a539d7a09097b1a08c2e2c
fares <fares002@example.com>
2024-05-01 16:02:25 -0700

    test

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -12,9 +12,31 @@ from models import storage
 @app_views.route("/status", methods=['GET'], strict_slashes=False)
 def status():
     """
-    status"""
+    status
+    """
     data = {
         "status": "OK"
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 200
+
+    return resp
+
+
+@app_views.route("/stats", methods=['GET'], strict_slashes=False)
+def stats():
+    """
+    Return the number of each object 
+    in the database
+    """
+    data = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User"),
     }
 
     resp = jsonify(data)
