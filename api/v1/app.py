fb6200e5feb9c8dcf4d4818919b1ff0cefc69440
Abeermohsien <aboorakhalil993@gmail.com>
2024-05-01 12:28:18 -0700

    update

---
+++ api/v1/app.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+app filestorage
+"""
+
+from flask import Flask, jsonify
+from flask_cors import CORS
+from os import getenv
+
+from api.v1.views import app_views
+from models import storage
+
+
+app = Flask(__name__)
+
+CORS(app, resources={r'api/v1/*': {"origins": "www.google.com"}})
+
+app.register_blueprint(app_views)
+app.url_map.strict_slashes = false
+
+@app.teardown_appcontext
+def teardown(exception):
+    """
+    close storage"""
+    storage.close()
+
+
+@app.errorhandler(404)
+def handle_404(exception):
+    """
+    handles error 404"""
+    data = {
+        "error": "Not found"
+    }
+
+    resp = jsonify(data)
+    return resp, 404
+
+
+if __name__ == "__main__":
+    Host = getenv('HBNH_API_HOST', '0.0.0.0')
+    PORT = int(getenv('HBNB_API_PORT', 5000))
+    app.run(debug=True, host=HOST, port=PORT, threaded=True)
