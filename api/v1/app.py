0ae7a52cff965f244663d7c21294bf99ca9db6c9
Abeermohsien <aboorakhalil993@gmail.com>
2024-05-01 16:10:43 -0700

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -41,4 +41,4 @@ def handle_404(exception):
 if __name__ == "__main__":
     HOST = getenv('HBNB_API_HOST', '0.0.0.0')
     PORT = int(getenv('HBNB_API_PORT', 5000))
-    app.run( host=HOST, port=PORT, threaded=True)
+    app.run(host=HOST, port=PORT, threaded=True)
