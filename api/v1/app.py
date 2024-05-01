0b32eda9de5ad0a49bf0359556c902fc48b5e99c
Abeermohsien <aboorakhalil993@gmail.com>
2024-05-01 16:15:39 -0700

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """
-app filestorage
+json
 """
 
-from flask import Flask, jsonify, Blueprint
+from flask import Flask, jsonify
 from flask_cors import CORS
 from os import getenv
 
@@ -13,32 +13,31 @@ from models import storage
 
 app = Flask(__name__)
 
-CORS(app, resources={r'api/v1/*': {"origins": "0.0.0.0"}})
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 app.register_blueprint(app_views)
-app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
 def teardown(exception):
     """
-    close storage"""
+    close"""
     storage.close()
 
 
 @app.errorhandler(404)
 def handle_404(exception):
     """
-    handles error 404"""
+    error"""
     data = {
         "error": "Not found"
     }
 
     resp = jsonify(data)
-    return resp, 404
+    resp.status_code = 404
+
+    return(resp)
 
 
 if __name__ == "__main__":
-    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
-    PORT = int(getenv('HBNB_API_PORT', 5000))
-    app.run(host=HOST, port=PORT, threaded=True)
+    app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
