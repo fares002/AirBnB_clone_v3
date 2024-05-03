26b9635ac5a0a0ccd69cc811f60f6e2781cda857
fares <fares002@example.com>
2024-05-02 17:06:31 -0700

    fixed

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -13,7 +13,7 @@ def get_states():
     """get all states"""
     states = storage.all(State).values()
     states = [state.to_dict() for state in states]
-    return jsonify(states)
+    return states
 
 
 @app_views.route("/states/<state_id>", methods=["GET"], strict_slashes=False)
@@ -22,7 +22,7 @@ def get_state(state_id):
     state = storage.get(State, state_id)
     if state is None:
         abort(404)
-    return jsonify(state.to_dict())
+    return state.to_dict()
 
 
 @app_views.route("/states/<state_id>", methods=["DELETE"])
@@ -46,7 +46,7 @@ def create_state():
         abort(400, 'Missing name')
     state = State(**data)
     storage.save()
-    return jsonify(state.to_dict()), 201
+    return state.to_dict(), 201
 
 
 @app_views.route("/states/<state_id>", methods=["PUT"], strict_slashes=False)
@@ -62,4 +62,4 @@ def update_state(state_id):
         if key not in ['id', 'created_at', 'updated_at']:
             setattr(state, key, value)
     storage.save()
-    return jsonify(state.to_dict())
+    return state.to_dict()
